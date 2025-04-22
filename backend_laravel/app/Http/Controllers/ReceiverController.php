8f316ec337e1f66ada253778059a215db14dc4c7
vungco <vung2k3@gmail.com>
2025-04-22 11:11:40 +0700

    backend migrate

---
+++ backend_laravel/app/Http/Controllers/ReceiverController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Receiver;
+use Illuminate\Http\Request;
+
+class ReceiverController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Receiver $receiver)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Receiver $receiver)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Receiver $receiver)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Receiver $receiver)
+    {
+        //
+    }
+}
